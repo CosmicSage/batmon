4fbd7a16edd20b5b5fae8c7d164f1182e547f87b
CosmicSage <prabhakarmishra9@yahoo.com>
2020-12-18 21:51:15 +0530

    some basics

--- Windows.cxx
+++ Windows.cxx
@@ -4,13 +4,22 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    SYSTEM_POWER_STATUS spsPwr;
-    if( GetSystemPowerStatus(&spsPwr) ) {
-        cout << "\nAC Status : " << static_cast<double>(spsPwr.ACLineStatus)
-             << "\nBattery Status : " << static_cast<double>(spsPwr.BatteryFlag)
-             << "\nBattery Life % : " << static_cast<double>(spsPwr.BatteryLifePercent)
-             << endl;
-        return 0;
+    SYSTEM_POWER_STATUS systemPower;
+    int x = 1;
+    // GEt Status
+    while (true) {
+      if( GetSystemPowerStatus(&systemPower) ) {
+        if (systemPower.BatteryLifePercent <= 53) {
+          cout << "\nAC Status : " << static_cast<double>(systemPower.ACLineStatus)
+          << "\nBattery Status : " << static_cast<double>(systemPower.BatteryFlag)
+          << "\nBattery Life % : " << static_cast<double>(systemPower.BatteryLifePercent)
+          << "\n" << systemPower.BatteryFullLifeTime
+          << endl;
+          return 0;
+
+      }
     } else return 1;
+      printf("%i\r", systemPower.BatteryLifePercent);
+    }
 }
-spsPwr.BatteryLifePercent holds re
+// systemPower.BatteryLifePercent holds re
