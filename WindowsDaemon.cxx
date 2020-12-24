11606ba1b0eec6266e168123a90119e4ea7bbe76
CosmicSage <prabhakarmishra9@yahoo.com>
2020-12-24 19:16:04 +0530

    Incredible simplification of previously muttonheaded approach to alarm

--- WindowsDaemon.cxx
+++ WindowsDaemon.cxx
@@ -10,36 +10,21 @@ int main(int argc, char *argv[]) {
     while (true) {
       if( GetSystemPowerStatus(&systemPower) ) {
 
+
         // All System Normal thus get battery percentage
         int BatteryPercentage = systemPower.BatteryLifePercent;
         int AC = systemPower.ACLineStatus;
 
+        if (BatteryPercentage >= 66 && AC == 1) {
+          Beep(523, 500);
+        }
         // Alert User to Unplug device
-        if (BatteryPercentage >= 26) {
+        if (BatteryPercentage >= 86) {
           cout << "\nAC Status : " << static_cast<double>(systemPower.ACLineStatus)
           << "\nBattery Status : " << static_cast<double>(systemPower.BatteryFlag)
           << "\nBattery Life % : " << BatteryPercentage
           << "\n" << systemPower.BatteryFullLifeTime
           << endl;
-          // while (!systemPower.ACLineStatus && x <= 10) {
-          // while (!systemPower.ACLineStatus) {
-          while (true) {
-            // if () {
-            SYSTEM_POWER_STATUS systemPower2;
-            printf("%i\n", static_cast<int>(systemPower2.ACLineStatus));
-            if (systemPower2.ACLineStatus != 0) {
-              printf("\n\n\n\n\n\n\nbreaking\n\n\n\n\n\n\n\n");
-              break;
-            }
-            printf("%i\n", systemPower.ACLineStatus);
-            // printf("%i\n", systemPower.ACLineStatus);
-            printf("%i\n", x);
-            x++;
-            //   break;
-            //   // return 0;
-            // }
-            // Beep(523, 500);
-          }
       }
 
       // Alert user to pluggin the device if battery low
---
+++ test.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  while(1) {
+    std::cout << "Batman" << '\n';
+  }
+  return 0;
+}
