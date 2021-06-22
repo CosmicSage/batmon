b60c4f42875d7a57ab5f52f59b3e2a191e468a1e
CosmicSage <prabhakarmishra9@yahoo.com>
2021-06-23 00:55:56 +0530

    A very windows reliant message box? might work on linux too..

---
+++ message.cxx
@@ -0,0 +1,15 @@
+#include "message.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+int messagex(void) {
+  std::cout << "some variable" << IDCANCEL <<'\n';
+  int msgboxID = MessageBox(NULL, "Something up with your charging", "BATMON", MB_ICONWARNING);
+  return msgboxID;
+}
+
+int main(void) {
+  messagex();
+  return 0;
+}
