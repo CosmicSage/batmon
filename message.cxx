de88ca0e448b897d3956efa631a46dd2a793a449
CosmicSage <prabhakarmishra9@yahoo.com>
2021-07-01 23:10:27 +0530

    basic changes in input/return types

--- message.cxx
+++ message.cxx
@@ -6,8 +6,8 @@
 using namespace std;
 
 int alertx(void);
-int messagex(void);
-void * __cdecl playsound(void* filename);
+void * __cdecl messagex(void *number);
+void * __cdecl playsound(void *filename);
 
 int main(void) {
   alertx();
@@ -17,18 +17,19 @@ int main(void) {
 int alertx(void) {
   // do some error handling
   // Now threads
-  pthread_t thread1;
+  pthread_t thread1, thread2;
 
-  int f1 = pthread_create(&thread1, NULL, playsound, (void *)"media/evac.wav");
+  int f1 = pthread_create(&thread1, NULL, playsound, (void *)"../media/DArkness_prototype.wav");
+  // int f2 = pthread_create(&thread2, NULL, messagex, NULL);
   pthread_join(thread1, NULL);
   printf("%i\n", f1);
   return 0;
 }
 
-int messagex(void) {
+void * __cdecl messagex(void *number) {
   std::cout << "some variable" << IDCANCEL <<'\n';
   int msgboxID = MessageBox(NULL, "Something up with your charging", "BATMON", MB_ICONWARNING);
-  return msgboxID;
+  return (void *)msgboxID;
 }
 
 void * __cdecl playsound(void* filename) {
--- message.h
+++ message.h
@@ -1,6 +1,6 @@
 #ifndef MESSAGE_H  /* Include guard */
 #define MESSAGE_H
 
-int messagex(void);  /* An example function declaration */
+int alertx(void);  /* An example function declaration */
 
 #endif // MESSAGE_H_
