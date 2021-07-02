0b571b2b34bbee1c26e7345fd3724ea8d248889f
CosmicSage <prabhakarmishra9@yahoo.com>
2021-07-02 21:08:38 +0530

    Unavoidable Minimalist SYS Modal

--- message.cxx
+++ message.cxx
@@ -19,8 +19,9 @@ int alertx(void) {
   // Now threads
   pthread_t thread1, thread2;
 
-  int f1 = pthread_create(&thread1, NULL, playsound, (void *)"/media/evac.wav");
+  int f1 = pthread_create(&thread1, NULL, playsound, (void *)"media/evac.wav");
   int f2 = pthread_create(&thread2, NULL, messagex, NULL);
+  // messagex(NULL);
   pthread_join(thread1, NULL);
   printf("%i\n", f1);
   return 0;
@@ -28,7 +29,7 @@ int alertx(void) {
 
 void * __cdecl messagex(void *number) {
   std::cout << "some variable" << IDCANCEL <<'\n';
-  int msgboxID = MessageBox(NULL, "Something up with your charging", "BATMON", MB_ICONWARNING);
+  int msgboxID = MessageBox(NULL, "Something up with your charging", "BATMON", MB_SYSTEMMODAL);
   return (void *)msgboxID;
 }
 
